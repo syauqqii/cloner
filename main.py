de3c240ea607bd237dae6f8e0bc2c602025b24cf
S Y A U Q I <57295427+syauqqii@users.noreply.github.com>
2024-01-25 19:20:02 +0700

    fix bug, add new display

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ FOLDER_VIDEO = "video"
 """
 KEYWORDS = ['=']
 
-# LINE: 67 (START - END)
+# LINE: 72 (START - END)
 CUSTOM_OPERATION = False # True / False, DEFAULT: False
 EXCEPTION_EXTENSION = ['.blablabla'] # Change blablabla to bebas terserah kamuh
 #   ======= END - CONFIGURATION =======
@@ -38,16 +38,21 @@ def clean_filename(url):
     url = unquote(url)
 
     filename = os.path.basename(urlparse(url).path)
-    valid_characters = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_")
+    valid_characters = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.")
+    
     cleaned_filename = ''.join(char if char in valid_characters else '_' for char in filename)
     cleaned_filename = '_'.join(part for part in cleaned_filename.split('_') if part)
+
+    if '.' in cleaned_filename and cleaned_filename.rsplit('.', 1)[-1].lower() in ['jpg', 'jpeg', 'png', 'gif', 'svg', 'mp4', 'webm', 'css', 'js', 'json']:
+        return cleaned_filename
+
     if not cleaned_filename.endswith('.'):
-        cleaned_filename += '.' + filename.split('.')[-1]
+        cleaned_filename += '.' + filename.split('.')[-1].lower()
 
     return cleaned_filename
 
 def save_files(folder, files):
-    global SKIP, BERHASIL, GAGAL, KEYWORDS, EXCEPTION_EXTENSION
+    global SKIP_E, SKIP_D, BERHASIL, GAGAL, KEYWORDS, EXCEPTION_EXTENSION
 
     if not os.path.exists(folder):
         os.makedirs(folder)
@@ -55,7 +60,7 @@ def save_files(folder, files):
     for idx, file_url in enumerate(files, start=1):
         if any(keyword in file_url for keyword in KEYWORDS):
             print(f' > SKIP : {file_url} => ["="]')
-            SKIP  += 1
+            SKIP_E  += 1
             continue
 
         response = requests.get(file_url)
@@ -74,7 +79,7 @@ def save_files(folder, files):
 
             if os.path.exists(file_path):
                 print(f' > SKIP : {file_path} => SUDAH ADA')
-                SKIP += 1
+                SKIP_D += 1
                 continue
 
             with open(file_path, 'wb') as file:
@@ -88,7 +93,7 @@ def save_files(folder, files):
 
 def main():
     try:
-        global SKIP, BERHASIL, GAGAL, FOLDER_CSS, FOLDER_IMG, FOLDER_SVG, FOLDER_JS, FOLDER_AUDIO, FOLDER_VIDEO
+        global SKIP_E, SKIP_D, BERHASIL, GAGAL, FOLDER_CSS, FOLDER_IMG, FOLDER_SVG, FOLDER_JS, FOLDER_AUDIO, FOLDER_VIDEO
 
         os.system("cls||clear")
 
@@ -130,18 +135,51 @@ def main():
 
         print()
 
-        save_files(FOLDER_AUDIO, audio_files)
-        save_files(FOLDER_CSS, css_files)
-        save_files(FOLDER_JS, javascript_files)
-        save_files(FOLDER_GIF, gif_files)
-        save_files(FOLDER_IMG, img_files)
-        save_files(FOLDER_SVG, svg_files)
-        save_files(FOLDER_VIDEO, video_files)
+        if audio_files:
+            save_files(FOLDER_AUDIO, audio_files)
 
-        print(f"\n # TOTAL FILE [{BERHASIL+GAGAL+SKIP}]")
+        if css_files:
+            save_files(FOLDER_CSS, css_files)
+
+        if javascript_files:
+            save_files(FOLDER_JS, javascript_files)
+
+        if gif_files:
+            save_files(FOLDER_GIF, gif_files)
+
+        if img_files:
+            save_files(FOLDER_IMG, img_files)
+
+        if svg_files:
+            save_files(FOLDER_SVG, svg_files)
+
+        if video_files:
+            save_files(FOLDER_VIDEO, video_files)
+
+        print(f"\n # TOTAL FILE [{BERHASIL+GAGAL+SKIP_E+SKIP_D}]")
         print(f"   - BERHASIL: {BERHASIL}")
         print(f"   - GAGAL   : {GAGAL}")
-        print(f"   - SKIP    : {SKIP}")
+        print("   - SKIP {")
+        print(f"      - EXCLUDING KEYWORD : {SKIP_E}")
+        print(f"      - DUPLICATED        : {SKIP_D}")
+        print("     }")
+        print("   - HASIL {")
+        if audio_files:
+            print(f"      - AUDIO : {len(list(set(audio_files)))} file")
+        if css_files:
+            print(f"      - CSS   : {len(list(set(css_files)))} file")
+        if javascript_files:
+            print(f"      - JS    : {len(list(set(javascript_files)))} file")
+        if gif_files:
+            print(f"      - GIF   : {len(list(set(gif_files)))} file")
+        if img_files:
+            print(f"      - IMG   : {len(list(set(img_files)))} file")
+        if svg_files:
+            print(f"      - SVG   : {len(list(set(svg_files)))} file")
+        if video_files:
+            print(f"      - VIDEO : {len(list(set(video_files)))} file")
+        print("     }")
+
     except KeyboardInterrupt:
         print()
         exit(1)
@@ -151,7 +189,8 @@ def main():
 
 if __name__ == '__main__':
     BERHASIL = 0
-    SKIP     = 0
+    SKIP_D   = 0
+    SKIP_E   = 0
     GAGAL    = 0
 
     main()
